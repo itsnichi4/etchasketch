6a034936232842acf68eb65e169d8286dba0041d
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-01-21 18:11:00 +0200

    Improve code readability and resolve eraser bug

--- script.js
+++ script.js
@@ -1,100 +1,154 @@
-let mouseValue = "click";
-let currentDrawMode = "click";
-let changeBackgroundColor;
-let squaresNumber = prompt('Input the number of squares you want for your canvas(30-60)')
-
-if (squaresNumber>=60 || 30>=squaresNumber) {alert("ERROR! I SAID MIN 30 MAX 60!")
-prompt('Input the number of squares you want for your canvas(30-60)')
-}
-
-let container = document.createElement('div');
-container.className = "container"
-let grid = document.createElement('div');
-grid.className = 'grid';
-prompt
-for (let i = 0; i < squaresNumber; ++i) {
-    var column = document.createElement('div'); // create columns
-    column.className = 'column';
-    for (let j = 0; j < squaresNumber; ++j) {
-        let row = document.createElement('div'); // create rows
-        row.className = 'row';
-        column.appendChild(row);
-    }
-    grid.appendChild(column);
-}
-document.body.appendChild(container);
-container.appendChild(grid)
-
-let innerDiv = document.createElement('div');
-innerDiv.className = 'inner-div';
-
-let buttonContainer = document.createElement('div');
-buttonContainer.className = 'button-container';
-buttonContainer.appendChild(document.getElementById("favcolor"))
-for (let i = 0; i < 5; i++) {
-    let button = document.createElement('button');
-    button.id = 'Button ' + (i + 1);
-    button.className = 'button';
-    buttonContainer.appendChild(button);
-}
-innerDiv.appendChild(buttonContainer);
-
-container.appendChild(innerDiv);
-
-document.getElementById("Button 1").innerText = "Color";
-document.getElementById("Button 2").innerText = "Clear";
-document.getElementById("Button 3").innerText = "Eraser";
-document.getElementById("Button 4").innerText = "DrawMode - Hover";
-document.getElementById("Button 5").innerText = "DrawMode - Click";
-
-let divs = document.querySelectorAll(".row");
 let colorClick = false;
 let eraserClick = false;
+let mouseValue = "click";
+let currentDrawMode = "mouseover";
+let changeBackgroundColor = null ;
+main()
+    ;
 
-document.getElementById("Button 1").addEventListener("click", colorClicked)
-function colorClicked() {
-    colorClick = true;
-    let colorValue = document.getElementById("favcolor").value;
-    changeBackgroundColor = e => { e.target.style.backgroundColor = colorValue; }
-    divs.forEach(div => {
-        div.removeEventListener(currentDrawMode, changeBackgroundColor);
-        div.addEventListener(mouseValue, changeBackgroundColor);
-    });
-    eraserClick = false;
+function getRowsCount() {
+    let squaresNumber = 0;
+    while (30 > squaresNumber || squaresNumber > 100) {
+        alert("ERROR! I SAID MIN 30 MAX 60!")
+        squaresNumber = prompt('Input the number of squares you want for your canvas(30-60)')
+    }
+
+    return squaresNumber;
+}
+function populateDOM(squaresNumber) {
+    let container = document.createElement('div');
+    container.className = "container"
+    let grid = document.createElement('div');
+    grid.className = 'grid';
+    let divs = [];
+    for (let i = 0; i < squaresNumber; ++i) {
+        var column = document.createElement('div'); // create columns
+        column.className = 'column';
+        for (let j = 0; j < squaresNumber; ++j) {
+            let row = document.createElement('div'); // create rows
+            row.className = 'row';
+            column.appendChild(row);
+            divs.push(row);
+        }
+        grid.appendChild(column);
+    }
+    document.body.appendChild(container);
+    container.appendChild(grid)
+
+    let innerDiv = document.createElement('div');
+    innerDiv.className = 'inner-div';
+
+    let buttonContainer = document.createElement('div');
+    buttonContainer.className = 'button-container';
+    innerDiv.appendChild(buttonContainer);
+
+    buttonContainer.appendChild(createClearButton());
+    buttonContainer.appendChild(createEraseButton(divs));
+    buttonContainer.appendChild(createColorSelectButton(divs));
+    buttonContainer.appendChild(createDrawModeHoverButton(divs));
+    buttonContainer.appendChild(createDrawModeClickButton(divs));
+
+    buttonContainer.appendChild(document.getElementById("favcolor"))
+
+
+
+    container.appendChild(innerDiv);
 }
 
-let clearDivs = document.getElementById("Button 2")
-clearDivs.onclick = function () { location.reload(true) }
-
-document.getElementById("Button 3").addEventListener("click", eraserClicked)
-function eraserClicked() {
-    eraserClick = true;
-    colorClick = false;
-    changeBackgroundColor = e => { e.target.style.backgroundColor = "white"; }
-    divs.forEach(div => {
-        div.removeEventListener(currentDrawMode, changeBackgroundColor);
-        div.addEventListener(mouseValue, changeBackgroundColor);
-    });
+function createClearButton() {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.innerText = "Clear";
+    button.onclick = () => location.reload(true);
+    return (button);
 }
 
-document.getElementById("Button 4").addEventListener("click", modifyDrawMode);
-document.getElementById("Button 5").addEventListener("click", modifyDrawModeClick);
-
-function modifyDrawMode() {
-    currentDrawMode = "mouseover";
-    mouseValue = "mouseover";
-    divs.forEach(div => {
-        div.removeEventListener("click", changeBackgroundColor);
-        div.addEventListener("mouseover", changeBackgroundColor);
+function createColorSelectButton(divs) {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.innerText = "Color";
+    button.addEventListener("click", () => {
+        initColorMode(divs);
     });
+
+    return (button);
 }
 
-function modifyDrawModeClick() {
-    currentDrawMode = "click";
-    mouseValue = "click";
-    divs.forEach(div => {
-        div.removeEventListener("mouseover", changeBackgroundColor);
-        div.addEventListener("click", changeBackgroundColor);
+function initColorMode(divs) {
+        colorClick = true;
+        let colorValue = document.getElementById("favcolor").value;
+        changeBackgroundColor = e => { e.target.style.backgroundColor = colorValue; }
+        divs.forEach(div => {
+            div.removeEventListener("mouseover", changeBackgroundColor);
+            div.removeEventListener("click", changeBackgroundColor);
+            div.addEventListener(mouseValue, changeBackgroundColor, {once});
+        });
+        eraserClick = false;
+
+    
+}
+
+
+function createEraseButton(divs) {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.innerText = "Eraser";
+    button.addEventListener('click', () => {
+        eraserClick = true;
+        colorClick = false;
+        changeBackgroundColor = e => { e.target.style.backgroundColor = null; }
+        divs.forEach(div => {
+            div.removeEventListener("mouseover", changeBackgroundColor);
+            div.removeEventListener("click", changeBackgroundColor);
+            div.addEventListener(mouseValue, changeBackgroundColor, {once});
+        });
     });
+
+    return (button);
+}
+
+function createDrawModeHoverButton(divs) {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.innerText = "Draw Hover";
+    button.addEventListener('click', () => {
+        currentDrawMode = "mouseover";
+        mouseValue = "mouseover";
+        divs.forEach(div => {
+            div.removeEventListener("click", changeBackgroundColor);
+            div.addEventListener("mouseover", changeBackgroundColor);
+        });
+    });
+    return (button);
+}
+
+function createDrawModeClickButton(divs) {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.innerText = "Draw Click";
+    button.addEventListener('click', () => {
+        currentDrawMode = "click";
+        mouseValue = "click";
+        divs.forEach(div => {
+            div.removeEventListener("mouseover", changeBackgroundColor);
+            div.addEventListener("click", changeBackgroundColor);
+        })
+    })
+    return (button);
+}
+
+function main() {
+
+
+    let squaresNumber = getRowsCount();
+
+
+    populateDOM(squaresNumber);
+
+
+
+
+
+
 }
 
